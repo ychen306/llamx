02924f2994b2dc53ac106d780e5c4829df56ed34
Krit Boonsiriseth <talkon@mit.edu>
2023-03-13 04:51:30 -0400

    Lower FMA + cleanup code

--- LLAMX.cpp
+++ LLAMX.cpp
@@ -32,13 +32,33 @@ static void buildPasses(PassBuilder &PB) {
       });
 }
 
-static Value *emitLoadStoreConfig(Value *Ptr, Constant *Reg,
-                                  IRBuilderBase &IRB) {
+static Value *emitConfig(std::list<std::pair<Value*, int>> inputs, IRBuilderBase &IRB) {
+  auto *Int64Ty = IRB.getInt64Ty();
+  Value *out = IRB.getInt64(0);
+  for (auto &input : inputs) {
+    out = IRB.CreateOr(out, IRB.CreateShl(input.first, input.second));
+  }
+  return out;
+}
+
+static Value *emitLoadStoreConfig(Value *Ptr, Value *Reg, IRBuilderBase &IRB) {
   auto *Int64Ty = IRB.getInt64Ty();
   auto *Addr = IRB.CreatePointerCast(Ptr, Int64Ty);
   auto *RegNum = IRB.CreateZExt(Reg, Int64Ty);
   auto *Operand = IRB.CreateOr(Addr, IRB.CreateShl(RegNum, 56), "myor");
   return Operand;
+}
+
+static Value *emitFMAConfig(Value *XReg, Value *YReg, Value *ZReg, 
+                            int OpType, int Mode, IRBuilderBase &IRB) {
+  auto *Int64Ty = IRB.getInt64Ty();
+  auto *XRegNum = IRB.CreateZExt(XReg, Int64Ty);
+  auto *YRegNum = IRB.CreateZExt(YReg, Int64Ty);
+  auto *ZRegNum = IRB.CreateZExt(ZReg, Int64Ty);
+  Value *OpTypeNum = IRB.getInt64(OpType);
+  Value *ModeNum = IRB.getInt64(Mode);
+  
+  return emitConfig({{XRegNum, 16}, {YRegNum, 6}, {ZRegNum, 20}, {OpTypeNum, 27}, {ModeNum, 63}}, IRB);
 }
 
 static void emitAMX(AMXOpcode Opcode, Value *Operand, IRBuilderBase &IRB) {
@@ -52,60 +72,54 @@ static void emitAMX(AMXOpcode Opcode, Value *Operand, IRBuilderBase &IRB) {
   IRB.CreateCall(IA, {ConstantInt::get(Int32Ty, Opcode), Operand});
 }
 
-static void lowerLDX(Constant *DestReg, Value *Ptr, Instruction *InsertBefore) {
-  errs() << "lowering LDX: dest reg = " << *DestReg << ", ptr = " << *Ptr
-         << '\n';
+static void lowerLoad(CallInst *CI, Instruction *InsertBefore, AMXOpcode Opcode) {
+  auto *DestReg = CI->getArgOperand(0);
+  auto *SrcPtr = CI->getArgOperand(1);
+
+  assert(DestReg->getType()->isIntegerTy(32));
+  // assert(isa<ConstantInt>(DestReg));
+  assert(SrcPtr->getType()->isPointerTy());
+
   IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::LDX, emitLoadStoreConfig(Ptr, DestReg, IRB), IRB);
+
+  emitAMX(Opcode, emitLoadStoreConfig(SrcPtr, (DestReg), IRB), IRB);
 }
 
-static void lowerLDY(Constant *DestReg, Value *Ptr, Instruction *InsertBefore) {
-  errs() << "lowering LDY: dest reg = " << *DestReg << ", ptr = " << *Ptr
-         << '\n';
+static void lowerStore(CallInst *CI, Instruction *InsertBefore, AMXOpcode Opcode) {
+  auto *DestPtr = CI->getArgOperand(0);
+  auto *SrcReg = CI->getArgOperand(1);
+
+  assert(SrcReg->getType()->isIntegerTy(32));
+  // assert(isa<ConstantInt>(SrcReg));
+  assert(DestPtr->getType()->isPointerTy());
+
   IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::LDY, emitLoadStoreConfig(Ptr, DestReg, IRB), IRB);
+
+  emitAMX(Opcode, emitLoadStoreConfig(DestPtr, (SrcReg), IRB), IRB);
 }
 
-static void lowerSTX(Value *Ptr, Constant *SrcReg, Instruction *InsertBefore) {
-  errs() << "lowering STX: dest ptr = " << *Ptr << ", src reg = " << *SrcReg
-         << '\n';
-  IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::STX, emitLoadStoreConfig(Ptr, SrcReg, IRB), IRB);
-}
+static void lowerFma(CallInst *CI, Instruction *InsertBefore, AMXOpcode Opcode) {
+  auto *XReg = CI->getArgOperand(1);
+  auto *YReg = CI->getArgOperand(2);
+  auto *ZReg = CI->getArgOperand(0);
 
-static void lowerSTY(Value *Ptr, Constant *SrcReg, Instruction *InsertBefore) {
-  errs() << "lowering STY: dest ptr = " << *Ptr << ", src reg = " << *SrcReg
-         << '\n';
-  IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::STY, emitLoadStoreConfig(Ptr, SrcReg, IRB), IRB);
-}
+  assert(XReg->getType()->isIntegerTy(32));
+  // assert(isa<ConstantInt>(XReg));
+  assert(YReg->getType()->isIntegerTy(32));
+  // assert(isa<ConstantInt>(YReg));
+  assert(ZReg->getType()->isIntegerTy(32));
+  // assert(isa<ConstantInt>(ZReg));
 
-static void lowerLDZ(Constant *DestReg, Value *Ptr, Instruction *InsertBefore) {
-  errs() << "lowering LDZ: dest reg = " << *DestReg << ", ptr = " << *Ptr
-         << '\n';
   IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::LDZ, emitLoadStoreConfig(Ptr, DestReg, IRB), IRB);
-}
 
-static void lowerSTZ(Value *Ptr, Constant *SrcReg, Instruction *InsertBefore) {
-  errs() << "lowering STZ: dest ptr = " << *Ptr << ", src reg = " << *SrcReg
-         << '\n';
-  IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::STZ, emitLoadStoreConfig(Ptr, SrcReg, IRB), IRB);
-}
-
-static void lowerLDZI(Constant *DestReg, Value *Ptr, Instruction *InsertBefore) {
-  errs() << "lowering LDZ: dest reg/half = " << *DestReg << ", ptr = " << *Ptr
-         << '\n';
-  IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::LDZI, emitLoadStoreConfig(Ptr, DestReg, IRB), IRB);
-}
-
-static void lowerSTZI(Value *Ptr, Constant *SrcReg, Instruction *InsertBefore) {
-  errs() << "lowering STZ: dest ptr = " << *Ptr << ", src reg/half = " << *SrcReg
-         << '\n';
-  IRBuilder<> IRB(InsertBefore);
-  emitAMX(AMXOpcode::STZI, emitLoadStoreConfig(Ptr, SrcReg, IRB), IRB);
+  emitAMX(Opcode, emitFMAConfig(
+    (XReg),
+    (YReg),
+    (ZReg),
+    0, // bit27-29
+    0, // bit63
+    IRB
+  ), IRB);
 }
 
 PreservedAnalyses AMXLowering::run(Function &F, FunctionAnalysisManager &AM) {
@@ -120,73 +134,27 @@ PreservedAnalyses AMXLowering::run(Function &F, FunctionAnalysisManager &AM) {
       if (!Callee || !Callee->getName().startswith("amx_"))
         continue;
       if (Callee->getName() == "amx_ldx") {
-        auto *DestReg = CI->getArgOperand(0);
-        auto *Ptr = CI->getArgOperand(1);
-
-        assert(DestReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(DestReg));
-        assert(Ptr->getType()->isPointerTy());
-
-        lowerLDX(cast<ConstantInt>(DestReg), Ptr, &I /*insert before*/);
+        lowerLoad(CI, &I, AMXOpcode::LDX);
       } else if (Callee->getName() == "amx_ldy") {
-        auto *DestReg = CI->getArgOperand(0);
-        auto *Ptr = CI->getArgOperand(1);
-
-        assert(DestReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(DestReg));
-        assert(Ptr->getType()->isPointerTy());
-
-        lowerLDY(cast<ConstantInt>(DestReg), Ptr, &I /*insert before*/);
+        lowerLoad(CI, &I, AMXOpcode::LDY);
       } else if (Callee->getName() == "amx_stx") {
-        auto *DestPtr = CI->getArgOperand(0);
-        auto *SrcReg = CI->getArgOperand(1);
-
-        assert(SrcReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(SrcReg));
-
-        lowerSTX(DestPtr, cast<ConstantInt>(SrcReg), &I /*insert before*/);
+        lowerStore(CI, &I, AMXOpcode::STX);
       } else if (Callee->getName() == "amx_sty") {
-        auto *DestPtr = CI->getArgOperand(0);
-        auto *SrcReg = CI->getArgOperand(1);
-
-        assert(SrcReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(SrcReg));
-
-        lowerSTY(DestPtr, cast<ConstantInt>(SrcReg), &I /*insert before*/);
+        lowerStore(CI, &I, AMXOpcode::STY);
       } else if (Callee->getName() == "amx_ldz") {
-        auto *DestReg = CI->getArgOperand(0);
-        auto *Ptr = CI->getArgOperand(1);
-
-        assert(DestReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(DestReg));
-        assert(Ptr->getType()->isPointerTy());
-
-        lowerLDZ(cast<ConstantInt>(DestReg), Ptr, &I /*insert before*/);
+        lowerLoad(CI, &I, AMXOpcode::LDZ);
       } else if (Callee->getName() == "amx_stz") {
-        auto *DestPtr = CI->getArgOperand(0);
-        auto *SrcReg = CI->getArgOperand(1);
-
-        assert(SrcReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(SrcReg));
-
-        lowerSTZ(DestPtr, cast<ConstantInt>(SrcReg), &I /*insert before*/);
+        lowerStore(CI, &I, AMXOpcode::STZ);
       } else if (Callee->getName() == "amx_ldzi") {
-        auto *DestReg = CI->getArgOperand(0);
-        auto *Ptr = CI->getArgOperand(1);
-
-        assert(DestReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(DestReg));
-        assert(Ptr->getType()->isPointerTy());
-
-        lowerLDZI(cast<ConstantInt>(DestReg), Ptr, &I /*insert before*/);
+        lowerLoad(CI, &I, AMXOpcode::LDZI);
       } else if (Callee->getName() == "amx_stzi") {
-        auto *DestPtr = CI->getArgOperand(0);
-        auto *SrcReg = CI->getArgOperand(1);
-
-        assert(SrcReg->getType()->isIntegerTy(32));
-        assert(isa<ConstantInt>(SrcReg));
-
-        lowerSTZI(DestPtr, cast<ConstantInt>(SrcReg), &I /*insert before*/);
+        lowerStore(CI, &I, AMXOpcode::STZI);
+      } else if (Callee->getName() == "amx_fma64") {
+        lowerFma(CI, &I, AMXOpcode::FMA64);
+      } else if (Callee->getName() == "amx_fma32") {
+        lowerFma(CI, &I, AMXOpcode::FMA32);
+      } else if (Callee->getName() == "amx_fma16") {
+        lowerFma(CI, &I, AMXOpcode::FMA16);
       } else {
         llvm_unreachable("don't know how to lower this intrinsic");
       }
--- amx.h
+++ amx.h
@@ -8,4 +8,7 @@ void amx_ldz(unsigned dst, void *src_ptr);
 void amx_stz(void *dst_ptr, unsigned src);
 void amx_ldzi(unsigned dst, void *src_ptr);
 void amx_stzi(void *dst_ptr, unsigned src);
+void amx_fma64(unsigned zcol, unsigned xreg, unsigned yreg); // Outer product, full fma only
+void amx_fma32(unsigned zcol, unsigned xreg, unsigned yreg); // Outer product, full fma only
+void amx_fma16(unsigned zcol, unsigned xreg, unsigned yreg); // Outer product, full fma only
 #endif // AMX_H
