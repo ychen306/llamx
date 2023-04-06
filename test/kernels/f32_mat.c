bf9220ba4da52de375ed09c283dcf524a5bafa95
Krit Boonsiriseth <talkon@mit.edu>
2023-04-06 13:10:07 -0400

    Update matrix multiplication kernels

---
+++ test/kernels/f32_mat.c
@@ -0,0 +1,62 @@
+// RUN: source path.sh
+// RUN: llamx-clang %s -c -O1 -I %S/../.. -o %t
+// RUN: cc %t -o %t
+// RUN: %t | FileCheck %s
+
+// CHECK: fma32_mat: PASS
+
+#include "aarch64.h"
+#include "amx.h"
+
+#include <stdio.h>
+#include <string.h>
+
+// Computes Z += Y^T@X
+void matmul32(float Z[256], float X[16][16], float Y[16][16]) {
+
+  AMX_SET();
+  amx_ldz_col2(65, Z);
+  for (int i = 0; i < 16; i++) {
+    amx_ldx(66, X[i]);
+    amx_ldy(67, Y[i]);
+    amx_fma32_mat(65, 66, 67);
+  }
+  amx_stz_col2(Z, 65);
+  AMX_CLR();
+}
+
+int main() {
+  float X[16][16];
+  float Y[16][16];
+  float Z[256];
+  float ref[256];
+
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 16; j++) {
+      X[i][j] = i + j;
+      Y[i][j] = i - j;
+      Z[16 * i + j] = i;
+      ref[16 * i + j] = 1240 + 120 * (j - i) - 16 * i * j + i;
+    }
+  }
+
+  matmul32(Z, X, Y);
+
+  printf("Z:\n");
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 16; j++) {
+      printf("%f ", Z[16 * i + j]);
+    }
+    printf("\n");
+  }
+
+  printf("ref:\n");
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 16; j++) {
+      printf("%f ", ref[16 * i + j]);
+    }
+    printf("\n");
+  }
+
+  printf("%s: %s\n", "fma32_mat", memcmp(Z, ref, 1024) ? "FAIL" : "PASS");
+}
--- test/kernels/f64_mat.c
+++ test/kernels/f64_mat.c
@@ -3,7 +3,7 @@
 // RUN: cc %t -o %t
 // RUN: %t | FileCheck %s
 
-// CHECK: matmul64: PASS
+// CHECK: fma64_mat: PASS
 
 #include "aarch64.h"
 #include "amx.h"
@@ -11,37 +11,36 @@
 #include <stdio.h>
 #include <string.h>
 
-// Computes Z = Y^T@X
-void matmul64(double Z[8][8], double X[8][8], double Y[8][8]) {
-  unsigned zcol = 3;
+// Computes Z += Y^T@X
+void matmul64(double Z[64], double X[8][8], double Y[8][8]) {
 
   AMX_SET();
+  amx_ldz_col(65, Z);
   for (int i = 0; i < 8; i++) {
-    amx_ldx(i, X[i]);
-    amx_ldy(i, Y[i]);
-    amx_fma64_mat(zcol, i, i);
+    amx_ldx(66, X[i]);
+    amx_ldy(67, Y[i]);
+    amx_fma64_mat(65, 66, 67);
   }
-  for (int j = 0; j < 8; j++) {
-    amx_stz(Z[j], 8 * j + zcol);
-  }
+  amx_stz_col(Z, 65);
   AMX_CLR();
 }
 
 int main() {
   double X[8][8];
   double Y[8][8];
-  double Z[8][8];
-  double ref[8][8];
+  double Z[64];
+  double ref[64];
 
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       X[i][j] = i + j;
       Y[i][j] = i - j;
-      ref[i][j] = 140 + 28 * (j - i) - 8 * i * j;
+      Z[8 * i + j] = i;
+      ref[8 * i + j] = 140 + 28 * (j - i) - 8 * i * j + i;
     }
   }
 
   matmul64(Z, X, Y);
 
-  printf("%s: %s\n", "matmul64", memcmp(Z, ref, 512) ? "FAIL" : "PASS");
+  printf("%s: %s\n", "fma64_mat", memcmp(Z, ref, 512) ? "FAIL" : "PASS");
 }
